dbd57d0e29e77bff49968e983bb31bd8543d5347
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-06-07 10:46:46 -0700

    both tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,6 +18,18 @@ describe('fs-backend-zodiac routes', () => {
     expect(res.body).toEqual(expected);
   });
 
+
+  it('zodiacs/:id should return zodiac detail', async () => {
+    const res = await request(app).get('/zodiacs/1');
+    const aquarius = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer'
+    };
+    expect(res.body).toEqual(aquarius);
+  });
+
   afterAll(() => {
     pool.end();
   });
